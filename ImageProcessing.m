130eae60f10e3fdc81973039d67c27252b189cab
rhennigan <rhennigan@gmail.com>
2015-05-30 02:13:10 -0400

    auto-commit changes: Sat May 30 02:13:11 EDT 2015

--- ImageProcessing.m
+++ ImageProcessing.m
@@ -5,6 +5,36 @@
 
 BeginPackage["GalaxyClassification`ImageProcessing`", {"GalaxyClassification`"}]
 
-Print[$ContextPath];
+selectGalaxy[img_, t_] := SelectComponents[FillingTransform@Binarize[img, t], "Area", -1]
+selectGalaxy[img_] := SelectComponents[FillingTransform@Binarize[img], "Area", -1]
+galaxyAngle[galaxyComponent_] := 1 /. ComponentMeasurements[galaxyComponent, "Orientation"]
+croppedCentroid[img_, s_] := ImageMeasurements[ImageCrop[img, s], "IntensityCentroid"] / s
+contrast[img_, exponent_] := ImageApply[Max[0.0, #]^exponent&, img]
+
+selectCenterGalaxy[img_, t_] := Module[
+  {components, label},
+  components = FillingTransform@Binarize[img, t];
+  label = MinimalBy[ComponentMeasurements[components, "Centroid"], Norm[#[[2]] - ImageDimensions[img] / 2]&][[1, 1]];
+  SelectComponents[components, "Label", # == label&]
+]
+selectCenterGalaxy[img_] := Module[
+  {components, label},
+  components = FillingTransform@Binarize[img];
+  label = MinimalBy[ComponentMeasurements[components, "Centroid"], Norm[#[[2]] - ImageDimensions[img] / 2]&][[1, 1]];
+  SelectComponents[components, "Label", # == label&]
+]
+
+transformImage[img_] := Module[{w, h, components, label, img2, imgc, orientation, rotated, cropped, resized},
+  {w, h} = ImageDimensions[img];
+  components = DeleteSmallComponents[FillingTransform@Dilation[AlphaChannel[RemoveBackground[img, {{{1, 1}, {w, 1}, {1, h}, {w, h}}, .025}]], 2]];
+  label = MinimalBy[ComponentMeasurements[components, "Centroid"], Norm[#[[2]] - ImageDimensions[img] / 2]&][[1, 1]];
+  img2 = ImageMultiply[img, Blur[SelectComponents[components, "Label", # == label&], 8]];
+  imgc = ImageCrop[img2, {w, h}];
+  orientation = galaxyAngle[selectGalaxy[imgc]];
+  rotated = ImageCrop[ImageRotate[imgc, -orientation], Min[w, h] / 2];
+  cropped = ImageTrim[rotated, {{-20, -20}, {20, 20}} + Round[1 /. ComponentMeasurements[selectCenterGalaxy[rotated, .05], "BoundingBox"]]];
+  resized = With[{m = Max[ImageDimensions[cropped]]}, ImageResize[ImageCrop[cropped, {m, m}], Min[w, h] / 2]];
+  resized
+]
 
 EndPackage[]
