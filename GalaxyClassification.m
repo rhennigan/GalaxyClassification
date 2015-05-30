deb9c0126230142578a40607a6b2bc4a62dcb538
rhennigan <rhennigan@gmail.com>
2015-05-30 01:26:30 -0400

    Initial commit

---
+++ GalaxyClassification.m
@@ -0,0 +1,22 @@
+(* Mathematica Package         *)
+(* Created by IntelliJ IDEA    *)
+
+(* :Title: GalaxyClassification     *)
+(* :Context: GalaxyClassification`  *)
+(* :Author: rhennigan            *)
+(* :Date: 5/30/2015              *)
+
+(* :Package Version: 1.0       *)
+(* :Mathematica Version:       *)
+(* :Copyright: (c) 2015 rhennigan *)
+(* :Keywords:                  *)
+(* :Discussion:                *)
+
+BeginPackage["GalaxyClassification`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"] (* Begin Private Context *)
+
+End[] (* End Private Context *)
+
+EndPackage[]
